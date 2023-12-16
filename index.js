9068e580dfa863960c9a3b28a07cc5331b240228
Swapnil-FWDeveloper <swapnilshende9284@gmail.com>
2023-12-16 19:04:15 +0530

    refactor :Created live clock for personal use

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 function updateClock() {
-    let now = new Date();
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
-    let ampm = hours >= 12 ? 'PM' : 'AM';
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  let ampm = hours >= 12 ? "PM" : "AM";
 
-    // Convert to 12-hour format
-    hours = hours % 12;
-    hours = hours ? hours : 12; // The hour '0' should be '12'
+  // Convert to 12-hour format
+  hours = hours % 12;
+  hours = hours ? hours : 12; // The hour '0' should be '12'
 
-    // Format the time
-    let timeString = formatTime(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds) + " " + ampm;
+  // Format the time
+  let timeString =
+    formatTime(hours) +
+    ":" +
+    formatTime(minutes) +
+    ":" +
+    ampm;
 
-    // Display the time in the 'time' element
-    document.getElementById('time').innerText = timeString;
+  // Display the time in the 'time' element
+  document.getElementById("time").innerText = timeString;
 
-    // Get the month and year
-    let month = now.toLocaleString('default', { month: 'long' });
-    let year = now.getFullYear();
+  // Get the month and year
+  let month = now.toLocaleString("default", { month: "long" });
+  let year = now.getFullYear();
 
-    // Display the date in the 'date' element
-    document.getElementById('date').innerText = month + ' ' + now.getDate() + ', ' + year;
+  // Display the date in the 'date' element
+  document.getElementById("date").innerText =
+    month + " " + now.getDate() + ", " + year;
 
-    // Update the clock every second
-    setTimeout(updateClock, 1000);
+  // Update the clock every second
+  setTimeout(updateClock, 1000);
 }
 
 // Function to format time (pad with zero)
 function formatTime(time) {
-    return time < 10 ? "0" + time : time;
+  return time < 10 ? "0" + time : time;
 }
 
 // themetoggle
@@ -41,4 +47,3 @@ function formatTime(time) {
 
 // Initial call to start the clock
 updateClock();
-
